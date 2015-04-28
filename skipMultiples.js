dac541157473208713404043fd122efd527ffc58
Shannon <shsulliv@gmail.com>
2015-04-28 00:15:56 -0300

    Delete skipMultiples.js

--- skipMultiples.js
+++
@@ -1,8 +0,0 @@
-function skipMultiples (x) {
-    for (x=1; x<=50;x++)
-        if (x=x*7) {
-            console.log("Be the change you want to see in the world"); 
-        } else {
-            console.log(x);
-        }
-};
